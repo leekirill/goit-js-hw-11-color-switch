3b6c62dcbfe0d47ae380238e55474ebb71acb793
Kirill Lee <cyril.lee1997@gmail.com>
2020-10-22 23:04:11 +0300

    task-1

---
+++ src/index.js
@@ -0,0 +1,32 @@
+const colors = [
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+];
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const bodyEl = document.querySelector('body')
+const btn = document.querySelectorAll('button')
+let intervalId = ''
+
+const btnActive = btn[0].addEventListener('click', (event) => {
+   intervalId = setInterval(() => {
+       bodyEl.style.backgroundColor = `${colors[randomIntegerFromInterval(0, 5)]}` 
+       if (intervalId) {
+           btn[0].setAttribute('disabled', 'true')
+} 
+}, 1000)
+})
+btn[1].addEventListener('click', (event) => {
+    clearInterval(intervalId)
+          if (event) {
+           btn[0].removeAttribute('disabled')
+} 
+})
+
